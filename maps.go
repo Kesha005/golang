fc47a60db7a6d00b6c7413a2826d5b87c374f532
Kerimberdi Saparow <keshasaparow@mail.ru>
2023-11-12 23:23:35 +0500

    map_test

--- maps.go
+++ maps.go
@@ -6,15 +6,17 @@ import (
 
 
 func  main(){
-	fmt.Println("It's maps")
 
-	m_a_p := map[int]string{
-        90: "Dog",
-        91: "Cat",
-        92: "Cow",
-        93: "Bird",
-        94: "Rabbit",
+    fmt.Println("It's a simple map")
+
+    m_a_p := map[int]string{
+        1:"Kerim",
+        2:"Kakamyrat",
+        3:"Selbi",
     }
- 
-	fmt.Println("And this is a map" + m_a_p[90])
+    for d , i:=range m_a_p{
+        fmt.Println("The index",d, "name",i)
+    }
+    
 }
+
