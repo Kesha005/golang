9cab3a642f2c6ceaa09bfc0658c83a22765ea23f
Kerimberdi Saparow <keshasaparow@mail.ru>
2023-11-29 23:52:12 +0500

    chanel_work

--- concurrency/mchanel.go
+++ concurrency/mchanel.go
@@ -1,30 +1,25 @@
 package main
-
-
 import "fmt"
 
+func main() {
 
+  // create channel
+  number := make(chan int)
+  message := make(chan string)
 
-func div(x,y int, c chan int)float64{
-	result := x/y
-	return float64(result)
+  // function call with goroutine
+  go channelData(number, message)
+
+  // retrieve channel data
+  fmt.Println("Channel Data:", <-number)
+  fmt.Println("Channel Data:", <-message)
+
 }
 
+func channelData(number chan int, message chan string) {
 
-func power(x,y int, c1 chan int)int{
-	rslt := x ^ y
-	return rslt
-}
-
-
-func main(){
-	fmt.Println("Enter two numbers to operate: ")
-	var x, y int
-	var c, c1 chan int
-	fmt.Scanln(&y)
-	fmt.Scanln(&x)
-
-	go div(x, y,c)
-	go power(x,y, c1)
+  // send data into channel
+  number <- 15
+  message <- "Learning Go channel"
 
 }
